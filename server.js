6d58cb08c0222d537f463a3f55736da7740d2a8f
Milivoj Kovacevic <milivoj.kovacevic@devtechgroup.com>
2014-10-14 17:03:05 +0200

    added everything manualy from c9

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-var express = require('express');
+var express = require("express");
+var mongoose = require("mongoose");
+
 
 var app = express();
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname, '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
-  res.render('index');
+app.get('/api/jobs', function(req, res) {
+    res.send('test');
 });
 
-app.listen(3000);
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+mongoose.connect('mongodb://localhost/jobfinder');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!');
+});
+
+app.listen(process.env.PORT, process.env.IP);
